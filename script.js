9de4744d71d7542c179ac71d05d9cc514f2d1ee5
ammarazahid <49521489+ammarazahid@users.noreply.github.com>
2019-07-23 06:50:12 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function one(){
+let click;
+
+function one() {
     // document.querySelector("#first").style.display = "none";
     document.querySelector("#choose").style.display = "block";
     document.querySelector("#Easy").style.display = "none";
@@ -6,7 +8,7 @@ function one(){
     document.querySelector("#hard").style.display = "none";
 }
 
-function easy(){
+function easy() {
     // document.querySelector("#first").style.display = "none";
     document.querySelector("#choose").style.display = "none";
     document.querySelector("#Easy").style.display = "block";
@@ -14,7 +16,7 @@ function easy(){
     document.querySelector("#hard").style.display = "none";
 }
 
-function medium(){
+function medium() {
     document.querySelector("#choose").style.display = "none";
     document.querySelector("#Easy").style.display = "none";
     document.querySelector("#Medium").style.display = "block";
@@ -22,7 +24,7 @@ function medium(){
 }
 
 
-function hard(){
+function hard() {
     document.querySelector("#choose").style.display = "none";
     document.querySelector("#Easy").style.display = "none";
     document.querySelector("#Medium").style.display = "none";
@@ -30,48 +32,53 @@ function hard(){
 }
 
 
-function checkEasy(){
-   let userNumber = document.querySelector("#easy-inp").value;
-   let randomIndex = (Math.random() * 50);
-   if (userNumber == randomIndex){
-       alert("Congratulations!! You guess right number....");
-   } else { 
-   if (userNumber > randomIndex){
-       alert("Too big");
-   } else if (userNumber < randomIndex){
-       alert("Too small");
-   }
-   }
+function checkEasy() {
+    let userNumber = document.querySelector("#easy-inp").value;
+    click++;
+    let randomIndex = parseInt((Math.random() * 50));
+    //    if (userNumber < 0){
+    //        alert("Please Enter a number");
+    //      } else {
+        if (click > 6){
+            alert("Game Over!!Try again");
+        } else{
+    if (userNumber == randomIndex) {
+        alert("Congratulations!! You guess right number....");
+    } else 
+        if (userNumber < randomIndex) {
+            alert("Try a bigger number");            
+        } else if (userNumber > randomIndex) {
+            alert("Try a smaller number");
+        }
+    }
 }
 
 
-function checkMedium(){
+function checkMedium() {
     let userNumber = document.querySelector("#med-inp").value;
-    let randomIndex = (Math.random() * 100);
-    if (userNumber == randomIndex){
+    let randomIndex = parseInt((Math.random() * 100));
+    if (userNumber == randomIndex) {
         alert("Congratulations!! You guess right number....");
-    } else { 
-    if (userNumber > randomIndex){
-        alert("Too big");
-    } else if (userNumber < randomIndex){
-        alert("Too small");
+    } else {
+        if (userNumber > randomIndex) {
+            alert("Try a bigger number");
+        } else if (userNumber < randomIndex) {
+            alert("Try a smaller number");
+        }
     }
-    }
- }
+}
 
- 
-function checkHard(){
+
+function checkHard() {
     let userNumber = document.querySelector("#hard-inp").value;
-    let randomIndex = (Math.random() * 1000);
-    if (userNumber == randomIndex){
+    let randomIndex = parseInt((Math.random() * 1000));
+    if (userNumber == randomIndex) {
         alert("Congratulations!! You guess right number....");
-    } else { 
-    if (userNumber > randomIndex){
-        alert("Too big");
-    } else if (userNumber < randomIndex){
-        alert("Too small");
+    } else {
+        if (userNumber > randomIndex) {
+            alert("Try a bigger number");
+        } else if (userNumber < randomIndex) {
+            alert("Try a smaller number");
+        }
     }
-    }
- }
- 
- 
+}
